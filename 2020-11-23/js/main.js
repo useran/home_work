08b19dc70efcf7c8983a5fc204b24bf7f6359055
Userand <kozlovskiyandriy@gmail.com>
2020-11-23 17:44:16 +0200

    upd3

--- 2020-11-23/js/main.js
+++ 2020-11-23/js/main.js
@@ -1,18 +1,18 @@
-let a = 2;
-let b = 5;
-let c = -3;
+let a = 1;
+let b = 4;
+let c = 4;
 let newVar= document.querySelector('.x');
 if (a!=0){
     let d = b*b - 4*a*c;
     if (d>0) {
         let x1 = (-b + Math.sqrt(d)) / 2*a;
         let x2 = (-b - Math.sqrt(d)) / 2*a;
-        newVar.innerHTML = 'a='+ a +'; '+'b='+b+'; '+'c='+c+'; '+ 'x1='+x1+'; '+'x2='+ x2;
+        newVar.innerHTML = 'a='+ a +'<br>'+'b='+ b +'<br>'+'c='+ c +'<br>'+ 'x1='+ x1 +'; '+'x2='+ x2;
     } else if (d==0) {
         let x = -b/(2*a);
-        newVar.innerHTML= 'a='+ a +'; '+'b='+b+'; '+'c='+c+'; '+ 'x='+x;
+        newVar.innerHTML= 'a='+ a +'<br>'+'b='+ b +'<br>'+'c='+ c +'<br>'+'x='+ x;
     } else if (d<0) {
-        newVar.innerHTML= 'a='+ a +'; '+'b='+b+'; '+'c='+c+'; ' + 'D<0 - Sorry, we cannot get x';
+        newVar.innerHTML= 'a='+ a +'<br>'+'b='+b+'<br>'+'c='+c+'<br>'+ 'D<0 - Sorry, we cannot get x';
     }
 } else {
     newVar.innerHTML= 'Sorry, this is not a quadratic equation';
