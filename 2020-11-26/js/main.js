993f5419d3c5544ce2bb4001c772c6b88b3d74cb
Userand <kozlovskiyandriy@gmail.com>
2020-11-25 23:04:32 +0200

    upl2

--- 2020-11-26/js/main.js
+++ 2020-11-26/js/main.js
@@ -11,8 +11,8 @@ let arr=[];
 let arr2=[];
 let n=9992100;
 let mynumber=9991999;
-let firstNum=0;
-let secondNum=0;
+let firstNum=null;
+let secondNum=null;
 number:
 for (let i = 9990100; i < n; i++) {
   for (let j = 2; j < i; j++) {
@@ -24,7 +24,7 @@ for (let i=0; i<arr.length; i++){
     arr2.push(Math.abs(arr[i]-mynumber));
 }
 let min = arr2[0];
-let index = 0;
+let index = null;
 for (let i=0; i<arr.length; i++){
   if (arr2[i] < min) {
     min = arr2[i];
