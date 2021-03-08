dae31360dd8337d671843dbba8902cf1d80e35e5
Userand <kozlovskiyandriy@gmail.com>
2021-03-08 15:16:59 +0200

    write file

--- Express_start/homework/29.02.2021/routes/index.js
+++ Express_start/homework/29.02.2021/routes/index.js
@@ -1,46 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const fsProm = require('fs').promises;
 const fs = require('fs');
-
-/* console.log(os.freemem()); */
+const os = require('os');
+const v8 = require('v8');
 
 router.get('/', (req, res) => {
   res.render('index');
 });
 
-//writing a file of 1e10 bytes size  
-const stream = fs.createWriteStream("C:/Users/kozlo/OneDrive/Desktop/sample.txt");
+console.time();
+console.log(os.freemem());
 
+let totalHeap = process.memoryUsage().heapTotal;
+let usedHeap = process.memoryUsage().heapUsed;
+let filePath = 'C:/Users/kozlo/OneDrive/Desktop/sample3.txt';  
+let str = '';
+let fileObj = Object;
+
+const writeFileFunc = async(str, path, qty) => {
+  fileObj = await fsProm.stat(path);
+  //building a string
+  while (usedHeap < totalHeap && str.length < qty) {
+    str = `${str}0`;
+    usedHeap = process.memoryUsage().heapUsed;
+  }
+  //append the given string to the file
+  while (fileObj.size < qty) {
+    fs.appendFile(path, str, err => { if (err) throw err });
+    fileObj = await fsProm.stat(path);
+  }
+  console.timeEnd();
+  console.log(os.freemem()); 
+}
+
+writeFileFunc(str, filePath, 1e10);
+
+/* const stream = fs.createWriteStream("C:/Users/kozlo/OneDrive/Desktop/sample3.txt");
 const writeChunk = async(data, times) => {
   for (let i=0; i<times; i++){
     if (!stream.write(data)){
-      await new Promise(resolve => stream.on('drain', resolve));
-    }
+      await new Promise(resolve => stream.once('drain', resolve));
+    } 
   }
-}
-writeChunk('0', 1e10);
-
-stream.on('finish', () => {
   console.log('All writes are complete!');
-});
-
-stream.on('error', function (err) {
-  console.log(err);
-});
-
-stream.close();
-
-/* console.log(os.freemem()); */
+  console.timeEnd();
+  console.log(os.freemem());
+}
+console.time();
+console.log(os.freemem());
+writeChunk('0', 1e10); */
 
 //uploading a file of no less than 2Gb size 
-router.post('/', (req, res) => {
+/* router.post('/', (req, res) => {
   const writeStream = fs.createWriteStream('./test.zip');
   req.pipe(writeStream);
   writeStream.close();
   res.sendStatus(200);
-/*   console.log(os.freemem()); */
-});
-
+  console.log(os.freemem());
+}); */
 module.exports = router;
 
 
