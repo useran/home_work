4c999d49b23624fec3b705c70a22f92eb40d4274
Userand <kozlovskiyandriy@gmail.com>
2021-03-17 12:52:02 +0200

    MySQL-CRUD3upd

--- Express_start/homework/18.03.2021/public/javascripts/main.js
+++ Express_start/homework/18.03.2021/public/javascripts/main.js
@@ -3,54 +3,56 @@ const formAdd = document.querySelector(".add");
 const formDel = document.querySelector(".delete");
 const answEl = document.querySelector(".output");
 
-//function for building an output string
-const thenFrame = r => {
+//function for axios handler 
+const axiosHandler = (url, data) => {
+  axios.post(url, data)
+    .then(thenOutStr) //building an output string-list of data gained from a database
+    .then(thenDelete) //adding "delete" functionality to the output list
+    .then(thenEdit) //adding "edit" option to the output list
+    .catch(e => answEl.innerHTML = `ERROR: ${e}`);
+}
+
+//building an output string/list
+const thenOutStr = r => {
   const resHtmlStr = r.data.reduce((htmlStr, el) => {
     return `${htmlStr}<li class='entries-${el.id}'>${el.id}. ${el.make} ${el.model} ${el.year} <button id='${el.id}' class='red-cross'>&#10060;</button> <button id='${el.id}' class='edit'>&#9998;</button></li>`;
   }, '');
   answEl.innerHTML = resHtmlStr;
 }
 
-//function for adding eventlistener to "close-cross" button and preparing a post send-out with axios
+//adding eventlisteners to "close-cross" button and preparing a post send-out with axios
 const thenDelete = () => {
   const bttns = document.querySelectorAll('.red-cross'); 
   bttns.forEach(el => {
     el.addEventListener('click', event => {
-      axios.post('/delete', { data: event.target.id })
-        .then(thenFrame)
-        .then(thenEdit)
-        .catch(e => answEl.innerHTML = `ERROR: ${e}`);
+      axiosHandler('/delete', { data: event.target.id })
     })
   })
 }
 
-//function for adding eventlistener to "edit" button and preparing an "update form" + adding event listener to the form
+//adding eventlisteners to "edit" button and preparing an "update form" + adding event listener to the form
 const thenEdit = () => {
   const editBtns = document.querySelectorAll('.edit'); 
-    editBtns.forEach(el => {
-      el.addEventListener('click', event => {
-        const currEntry = document.querySelector(`.entries-${event.target.id}`).innerHTML;
-        const entryArr = currEntry.slice(0, currEntry.indexOf(' <')).split(' ');
-        document.querySelector(`.entries-${event.target.id}`).innerHTML =
-          `${entryArr[0]}
-          <form class="update">
-            <input type="text" style="display:none" name="id" value="${event.target.id}">
-            <input type="text" name="make" value="${entryArr[1]}">
-            <input type="text" name="model" value="${entryArr[2]}">
-            <input type="text" name="year" value="${entryArr[3]}">
-            <input type="submit" value="Update">
-          </form>`
-        const formUpd = document.querySelector(".update");
-        formUpd.addEventListener("submit", function(event) {
-          event.preventDefault();
-          const data = new FormData(formUpd);
-          axios.post('/update', data)
-            .then(thenFrame)
-            .then(thenDelete)
-            .then(thenEdit)
-            .catch(e => answEl.innerHTML = `ERROR: ${e}`);
-        });
-      })
+  editBtns.forEach(el => {
+    el.addEventListener('click', event => {
+      const currEntry = document.querySelector(`.entries-${event.target.id}`).innerHTML;
+      const entryArr = currEntry.slice(0, currEntry.indexOf(' <')).split(' ');
+      document.querySelector(`.entries-${event.target.id}`).innerHTML =
+        `${entryArr[0]}
+        <form class="update">
+          <input type="text" style="display:none" name="id" value="${event.target.id}">
+          <input type="text" name="make" value="${entryArr[1]}">
+          <input type="text" name="model" value="${entryArr[2]}">
+          <input type="text" name="year" value="${entryArr[3]}">
+          <input type="submit" value="Update">
+        </form>`
+      const formUpd = document.querySelector(".update");
+      formUpd.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const data = new FormData(formUpd);
+        axiosHandler('/update', data)
+      });
+    })
   })
 }
 
@@ -58,20 +60,12 @@ const thenEdit = () => {
 formSel.addEventListener("submit", function(event) {
   event.preventDefault();
   const data = new FormData(formSel); 
-  axios.post('/filter', data)
-    .then(thenFrame)
-    .then(thenDelete)
-    .then(thenEdit)
-    .catch(e => answEl.innerHTML = `ERROR: ${e}`);
+  axiosHandler('/filter', data)
 });
 
 //event listener for "insert-form"
 formAdd.addEventListener("submit", function(event) {
   event.preventDefault();
-  const data = new FormData(formAdd); 
-  axios.post('/insert', data)
-    .then(thenFrame)
-    .then(thenDelete)
-    .then(thenEdit)
-    .catch(e => answEl.innerHTML = `ERROR: ${e}`);
+  const data = new FormData(formAdd);
+  axiosHandler('/insert', data) 
 });
