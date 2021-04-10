1f18913ebad12d4f9f02d41e20e1ec31ed50d37d
Userand <kozlovskiyandriy@gmail.com>
2021-04-10 13:39:15 +0300

    ajv+Front

--- Express_start/homework/10.04.2021/config/db.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    uri: 'mongodb://localhost:27017/Blog',
-    options: {
-      useUnifiedTopology: true, 
-      useNewUrlParser: true, 
-      useFindAndModify: false,
-      ignoreUndefined: true
-    }
-  }
--- Express_start/homework/10.04.2021/model/Articles/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const articleSchema = require('./schema.js');
-const path = require('path');
-
-const modelname = path.basename(__dirname);
-const Article = mongoose.model(modelname, articleSchema);
-
-module.exports = Article;
