161303a659da7934a3ffaf5507c875bd4d3208d0
Userand <kozlovskiyandriy@gmail.com>
2021-04-14 21:45:35 +0300

    getter/setter(changed date)

--- Express_start/homework/15.04.2021/model/Users/index.js
+++ Express_start/homework/15.04.2021/model/Users/index.js
@@ -3,16 +3,53 @@ const generalSchema = require('./schema.js');
 const path = require('path');
 const moment = require('moment');
 
+// search by salary
+generalSchema.statics.findBySalary = async function(from, to) {
+  const users = await this.find({ salary: { '$gte': from, '$lte': to }}, {__v:0});
+  return users;
+}
 
+// adding new entry
+generalSchema.statics.addNewUser = async function(data) {
+  try {
+    await this.create(data);
+    return 'Went OK!'
+  } catch(err) {
+    return(err.message);
+  }
+}
+
+// search by email and delete the entry
+generalSchema.statics.findByEmailAndDelete = async function(email) {
+  await this.deleteOne({ email: email });
+  return;
+}
+
+// find by Id and update
+generalSchema.statics.findByIdAndUpd = async function(id, obj) {
+  await this.findByIdAndUpdate({_id: id }, obj);
+  return;
+}
+
+// find by dayOfBirth
+generalSchema.statics.findByDoB = async function(from, to) {
+  const timeNow = moment().format('YYYY-MM-YY').split('-');
+  const dateTo = moment(`${Number(timeNow[0]) - from}-${timeNow[1]}-${timeNow[2]}`).toISOString();
+  const dateFrom = moment(`${Number(timeNow[0]) - to}-${timeNow[1]}-${timeNow[2]}`).toISOString();
+  const users = await this.find({ dOb: { '$gte': dateFrom, '$lte': dateTo }}, {__v:0});
+  return users;
+}
 //set-up of age getter&setter
 generalSchema.virtual('age')
   .get(function() {
-    const timeNow = moment().valueOf();
-    return (timeNow - moment(this.dOb).valueOf())/31556952000;
+    const timeNow = moment([]);
+    const dOb = moment([moment(this.dOb).format('YYYY-MM-DD').split('-')]);
+    return timeNow.diff(dOb, 'years');
   })
   .set(function(age){
-    const timeNow = moment().valueOf();
-    this.dOb = moment(timeNow - age*31556952000).toISOString();
+    const timeNow = moment().format('YYYY-MM-YY').split('-');
+    const diff = Number(timeNow[0]) - age;
+    this.dOb = moment(`${diff}-${timeNow[1]}-${timeNow[2]}`).toISOString();
   })
 
 generalSchema.statics.getUsersByAge = async function(from, to) {
--- Express_start/homework/15.04.2021/routes/index.js
+++ Express_start/homework/15.04.2021/routes/index.js
@@ -9,6 +9,14 @@ const controllers = require('../controllers/control.js');
 
 router.get('/', controllers.getUsers);
 
+router.post('/addUser', upload.none(), controllers.addUser);
+
+router.post('/salary', upload.none(), controllers.getUserBySalary);
+
+router.post('/email', upload.none(), controllers.deleteUserByEmail);
+
+router.post('/update', upload.none(), controllers.findByIdUpd);
+
 router.post('/dOb', upload.none(), controllers.findByAge);
 
 router.post('/set', upload.none(), controllers.setByAge);
