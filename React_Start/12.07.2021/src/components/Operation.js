67830223832cbb40753852fb8862a089538c2c48
Userand <kozlovskiyandriy@gmail.com>
2021-07-11 18:07:15 +0300

    calculator_update

--- React_Start/12.07.2021/src/components/Operation.js
+++ React_Start/12.07.2021/src/components/Operation.js
@@ -10,7 +10,12 @@ function Operation({ setRes, input1, input2 }) {
 
   const rendDivEl = () => {
     const arr = ['+', '-', '*', '/'];
-    const newArr = arr.map(el => <button className='oper' onClick={()=>setRes(mathOperations[el](input1, input2))}>{el}</button>)
+    const newArr = arr.map(el => <button id={el} className='oper' onClick={() => {
+      const btnArr = document.querySelectorAll('.oper');
+      btnArr.forEach(el => el.style.backgroundColor = 'white');
+      setRes(mathOperations[el](input1, input2));
+      document.getElementById(el).style.backgroundColor = 'orange';
+    }}>{el}</button>)
     return newArr;
   }
 
