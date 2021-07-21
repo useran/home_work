fbf54c6bbedfc1fb3cf0b3ec27f2ccec770eccaa
Userand <kozlovskiyandriy@gmail.com>
2021-07-21 22:43:29 +0300

    hex added

--- React_Start/19.07.2021/src/components/App.js
+++ React_Start/19.07.2021/src/components/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import React, { useState } from 'react';
-import Colors from './data.json';
 import InputRange from './InputRange';
 
 
 function App() {
-  const keyArr = Object.keys(Colors);
-  const maxRange = keyArr.length - 1;
-
+  const maxRange = 16777215;
   const [color, setColor] = useState(Math.floor(Math.random() * maxRange));
 
   return (
     <div className = 'App'>
-      <InputRange setColor={setColor} color={color} keyArr={keyArr} />
+      <InputRange setColor={setColor} color={color} maxRange={maxRange} />
     </div>
   );
 }
--- React_Start/19.07.2021/src/components/InputRange.js
+++ React_Start/19.07.2021/src/components/InputRange.js
@@ -1,24 +1,52 @@
 import Colors from './data.json';
 import './InputRange.css';
 
-function InputRange({setColor, color, keyArr}) {
-  
+function InputRange({setColor, color, maxRange}) {
+
   const showCurrColor = index => { 
+    index = `#${Number(index).toString(16).padStart(6, '0')}`;
     return <div className='container'>
       current: 
-      <input value={Colors[keyArr[index]]} style={{marginLeft:'30px'}}/>
-      <input style={{backgroundColor: keyArr[index]}}/>
+      <input value={index} style={{marginLeft:'30px'}}/>
+      <input type='color' value={index} />
     </div>
   };
 
-  const showNeighbors = index => { 
-    const leftKey = index - 1 < 0 ? 'No neighbor' : keyArr[index - 1];
-    const rightKey = Number(index) + 1 > keyArr.length-1 ? 'No neighbor' : keyArr[Number(index) + 1];
+  const showNeighbors = index => {
+    const valuesToInt = Object.values(Colors).map(el => parseInt(el.slice(1), 16));
+    const neighbors = { left: {}, right: {} };
+
+    //getting the left neighbor by searching for a negative difference between int values
+    const diffNegativeArr = [];
+    valuesToInt.forEach((el, i) => {
+      if (el - index < 0) {
+        diffNegativeArr[i] = el - index;
+      } else {
+        diffNegativeArr[i] = -1e8; //any number that won't be the max value in the array
+      }
+    })
+    const leftEl = diffNegativeArr.indexOf(Math.max(...diffNegativeArr));
+
+    //getting the right neighbor by searching for a positive difference between int values
+    const diffPositiveArr = [];
+    valuesToInt.forEach((el, i) => {
+      if (el - index > 0) {
+        diffPositiveArr[i] = el - index;
+      } else {
+        diffPositiveArr[i] = 1e8; //any number that won't be the min value in the array
+      }
+    })
+    const rightEl = diffPositiveArr.indexOf(Math.min(...diffPositiveArr));
+    
+    neighbors.left.code = index - 1 < 0 ? 'No neighbor' : Object.values(Colors)[leftEl];
+    neighbors.left.name = index - 1 < 0 ? '' : Object.keys(Colors).find(key => Colors[key] === neighbors.left.code);
+    neighbors.right.code = Number(index) + 1 > maxRange ? 'No neighbor' : Object.values(Colors)[rightEl]; 
+    neighbors.right.name = Number(index) + 1 > maxRange ? '' : Object.keys(Colors).find(key => Colors[key] === neighbors.right.code);
 
     return <div className='container'>
         neighbors: 
-        <input value={`${leftKey} ${Colors[leftKey]}`} style={{marginLeft:'10px'}} />
-        <input value={`${rightKey} ${Colors[rightKey]}`} />
+        <input value={`${neighbors.left.name} ${neighbors.left.code}`} style={{marginLeft:'10px'}} />
+        <input value={`${neighbors.right.name} ${neighbors.right.code}`} />
       </div>
   };
 
@@ -30,13 +58,14 @@ function InputRange({setColor, color, keyArr}) {
     <div className='wrap-container'>
       <input 
       type='range' 
-      min='0' max={`${keyArr.length-1}`}
+      min='0' max={maxRange}
       value={color} 
       onInput={handleChange}
       step='1'
       />
       {showCurrColor(color)}
       {showNeighbors(color)}
+
     </div>
   );
 }
